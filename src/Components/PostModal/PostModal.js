b69a9b32e5a486aec01a9adda022fdb1877633db
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-09-02 15:38:08 -0700

    post modal front end done

--- src/Components/PostModal/PostModal.js
+++ src/Components/PostModal/PostModal.js
@@ -1,13 +1,27 @@
 import styled from "styled-components";
+import closeIcon from "../../Assets/Svg/close-icon.svg"
+import user from "../../Assets/Svg/user.svg"
+
 
 const PostModal = (props) => {
     return <Container> 
         <Content>
-            Content
+            <Header> 
+                <h2>Create a new post</h2>
+                <button>
+                    <img src={closeIcon} alt="" />
+                </button>
+            </Header>
+            <SharedContent>
+                <UserInfo>
+                    <img src={user} alt="" />
+                    <span>Name</span>
+                </UserInfo>
+            </SharedContent>
         </Content>
          </Container>
     };
-
+ 
 
     const Container = styled.div`
     position: fixed;
@@ -31,8 +45,65 @@ const Content = styled.div`
     flex-direction: column;
     top: 32px;
     margin: 0 auto;
+`
+
+const Header = styled.div`
+    display: block;
+    padding: 16px 20px;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+    font-size: 15px;
+    line-height: 1.5;
+    color: rgba(0, 0, 0, 0.6);
+    font-weight: 400;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;   
+        button{
+            height: 40px;
+            width: 40px;
+            min-width: auto;
+            color: rgba(0, 0, 0, 0.6);
+            border: none;
+            border-radius: 50px;
+                svg{
+            pointer-events: none;
+        
+        }
+        }
+`
+
+const SharedContent = styled.div`
+        display: flex;
+        flex-direction: column;
+        flex-grow: 1;
+        overflow-y: auto;
+        vertical-align: baseline;
+        background: transparent;
+        padding: 8px 12px;
+`
+
+const UserInfo = styled.div`
+        display: flex;
+        align-items: center;
+        padding: 12px 24px;
+            svg, 
+            img{
+                width: 48px;
+                height: 48px;
+                background-clip: content-box;
+                border: 2px solid transparent;
+                border-radius: 50%;
+            }
+        span {
+            font-weight: 600;
+            font-size: 16px;
+            line-height: 1.5;
+            margin-left: 5px;
+        }
 
 `
+
+
 
     export default PostModal;
 
